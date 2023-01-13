afe69c06fcc01fa103e1819b0c2d6e9c5ffd47b1
AngeloVJ <angelovaldiviajara@gmail.com>
2023-01-12 21:05:22 -0500

    moon

--- src/main.js
+++ src/main.js
@@ -4,3 +4,18 @@ const menu = document.querySelector(".menu");
 iconMenu.addEventListener("click", function () {
     menu.classList.toggle("menu__show");
 });
+
+//moon turn on off
+const moon__dark = document.querySelector(".bx-moon");
+const moon__light = document.querySelector(".bxs-moon")
+
+moon__dark.addEventListener('click', function(){
+    moon__dark.classList.toggle('moon__none')
+    moon__light.classList.toggle('moon__block')
+    document.body.classList.toggle('darkmode')
+})
+moon__light.addEventListener('click', function(){
+    moon__light.classList.toggle('moon__block')
+    moon__dark.classList.toggle('moon__none')
+    document.body.classList.toggle('darkmode')
+})
